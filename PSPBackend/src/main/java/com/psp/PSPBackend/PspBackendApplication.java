2415574bbb7529c460ea34fff793498d5aa799cb
“BrankaKljajic” <branka.kljajic00@gmail.com>
2023-11-13 19:30:00 +0100

    Created initial psp backend project

---
+++ PSPBackend/src/main/java/com/psp/PSPBackend/PspBackendApplication.java
@@ -0,0 +1,13 @@
+package com.psp.PSPBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspBackendApplication.class, args);
+	}
+
+}
---
+++ PSPBackend/src/test/java/com/psp/PSPBackend/PspBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psp.PSPBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
