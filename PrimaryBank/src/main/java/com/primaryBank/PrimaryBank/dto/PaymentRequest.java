e3ee3bcb36ea35bcbc859c39067562a5a1d3c3e7
Milina Bucko <milina.bph@gmail.com>
2023-11-21 11:36:11 +0100

    paymentRequest validation

--- PrimaryBank/src/main/java/com/primaryBank/PrimaryBank/dto/PaymentRequest.java
+++ PrimaryBank/src/main/java/com/primaryBank/PrimaryBank/dto/PaymentRequest.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Pattern;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -12,8 +14,16 @@ import lombok.Setter;
 public class PaymentRequest {
 
     private Integer paymentId;
+
+    @Pattern(regexp = "^\\d{16}$", message = "Card number must be 16 digits")
     private String pan;
+
+    @Pattern(regexp = "^(0[1-9]|1[0-2])\\/\\d{2}$", message = "Invalid expiration date format (MM/YY)")
     private String expDate;
+
+    @Pattern(regexp = "^\\d{3}$", message = "CVV must be 3 digits")
     private String cvv;
+
+    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Cardholder name must contain only letters and spaces")
     private String cardHolderName;
 }
