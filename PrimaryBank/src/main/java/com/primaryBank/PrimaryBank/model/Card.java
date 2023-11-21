730dd86a6b84b2dceb2744c80772086783d41bc0
Milina Bucko <milina.bph@gmail.com>
2023-11-21 11:14:21 +0100

    card form validation on BE

--- PrimaryBank/src/main/java/com/primaryBank/PrimaryBank/model/Card.java
+++ PrimaryBank/src/main/java/com/primaryBank/PrimaryBank/model/Card.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Pattern;
+
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -18,8 +21,16 @@ public class Card {
 
     @Id
     private String id;
+
+    @Pattern(regexp = "^\\d{16}$", message = "Card number must be 16 digits")
     private String pan;
+
+    @Pattern(regexp = "^(0[1-9]|1[0-2])\\/\\d{2}$", message = "Invalid expiration date format (MM/YY)")
     private String expDate;
+
+    @Pattern(regexp = "^\\d{3}$", message = "CVV must be 3 digits")
     private String cvv;
+
+    @Pattern(regexp = "^[a-zA-Z\\s]*$", message = "Cardholder name must contain only letters and spaces")
     private String cardHolderName;
 }
