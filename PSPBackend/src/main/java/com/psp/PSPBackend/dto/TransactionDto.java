692a2d1462d1ca44e3f47f98d5534cff6ee113a1
“BrankaKljajic” <branka.kljajic00@gmail.com>
2023-11-21 01:12:49 +0100

    Scheduler changed

--- PSPBackend/src/main/java/com/psp/PSPBackend/dto/TransactionDto.java
+++ PSPBackend/src/main/java/com/psp/PSPBackend/dto/TransactionDto.java
@@ -1,2 +1,24 @@
-package com.psp.PSPBackend.dto;public class TransactionDto {
+package com.psp.PSPBackend.dto;
+
+import com.psp.PSPBackend.enums.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TransactionDto {
+    private Integer paymentId;
+    private Integer merchantOrderId;
+    private String merchantId;
+    private double amount;
+    private LocalDateTime merchantTimeStamp;
+    private PaymentStatus paymentStatus;
+    private LocalDateTime acquiererTimestamp;
+    private LocalDateTime issuerTimestamp;
 }
--- PSPBackend/src/main/java/com/psp/PSPBackend/service/BankScheduler.java
+++ PSPBackend/src/main/java/com/psp/PSPBackend/service/BankScheduler.java
@@ -1,2 +1,24 @@
-package com.psp.PSPBackend.service;public class BankScheduler {
+package com.psp.PSPBackend.service;
+
+import com.psp.PSPBackend.dto.TransactionDto;
+import com.psp.PSPBackend.webClient.PrimaryBankClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BankScheduler {
+
+    @Autowired
+    private PrimaryBankClient primaryBankClient;
+    @Scheduled(fixedRate = 3600000) // 1 sat = 3600000 milisekundi
+    public void schedulePrimaryBankTask() {
+        // Poziva servis svaki sat
+        List<TransactionDto> transactions = primaryBankClient.getTransactions();
+        for (TransactionDto transaction: transactions) {
+            // dobavi transakcioju po merchantOrderId i izmenim mu payment status
+        }
+    }
 }
