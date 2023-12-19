2da67673fd8d79b5f88b3b849e22a5f80d8f58c3
“BrankaKljajic” <branka.kljajic00@gmail.com>
2023-12-19 21:27:35 +0100

    Added service registry

--- PrimaryBank/src/main/java/com/primaryBank/PrimaryBank/PrimaryBankApplication.java
+++ PrimaryBank/src/main/java/com/primaryBank/PrimaryBank/PrimaryBankApplication.java
@@ -2,8 +2,10 @@ package com.primaryBank.PrimaryBank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PrimaryBankApplication {
 
 	public static void main(String[] args) {
