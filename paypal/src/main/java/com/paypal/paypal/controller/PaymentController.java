2782e89d3efef9fcf0d77a2316eaf204d90b6add
Milina Bucko <milina.bph@gmail.com>
2023-12-24 13:58:19 +0100

    feature: paypal payment

--- paypal/src/main/java/com/paypal/paypal/controller/PaymentController.java
+++ paypal/src/main/java/com/paypal/paypal/controller/PaymentController.java
@@ -2,19 +2,31 @@ package com.paypal.paypal.controller;
 
 import com.paypal.paypal.dto.AuthRequest;
 import com.paypal.paypal.dto.AuthResponse;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.paypal.paypal.model.CompletedOrder;
+import com.paypal.paypal.model.PaymentOrder;
+import com.paypal.paypal.service.PaymentService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/payment")
 @CrossOrigin(origins = "http://localhost:4200")
 public class PaymentController {
 
+    private PaymentService paymentService;
+
     @PostMapping(path = "/auth")
     public AuthResponse auth(@RequestBody AuthRequest authRequest) {
         return new AuthResponse(-1, "success paypal", 0.0);
+    }
+
+    @PostMapping(value = "/init")
+    public PaymentOrder createPayment(
+            @RequestParam("sum") Double sum) {
+        return paymentService.createPayment(sum);
+    } // endpoint koji ce da gadja PSP frontend u momentu - kupi (potrebno da salje amount)
+
+    @PostMapping(value = "/capture")
+    public CompletedOrder completePayment(@RequestParam("token") String token) {
+        return paymentService.completePayment(token);
     }
 }
